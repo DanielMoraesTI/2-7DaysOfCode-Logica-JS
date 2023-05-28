82a12238b557795f3cef299f3068293fbe15502f
Daniel Moraes <124385951+DanielMoraesTI@users.noreply.github.com>
2023-05-28 13:50:06 +0100

    Colocando modelo feito antes codepen por mim

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 console.log('Qual o seu nome?');
 console.log('Quantos anos você tem?');
 console.log('Qual linguagem de programação você está estudando?')
+
+
+//var numeroSecreto = parseInt(Math.random() * 1001 + 1)
+//var quantidadeTentativas = 1
+//var jogador = prompt('Qual é o seu nome?')
+//while(chute != numeroSecreto) {
+  //var chute = prompt('Digite um número entre 1 e 1000')
+  //if (chute == numeroSecreto) {
+    //alert('Certa resposta ' + jogador + ', agora pode tentar a sorte na Mega Sena! Volte sempre e me deixem críticas contrutivas, obrigado!')
+  //} else if (chute > numeroSecreto) {
+    //alert('Errou... o número secreto é menor do que ' + chute + ', tente novamente!')
+  //} else if (chute < numeroSecreto) {
+    //alert('Errou... o número secreto é maior do que ' + chute + ', tente novamente!')
+  //}
+  //alert('Quantidade de tentaivas: '+ quantidadeTentativas++)
+//}
