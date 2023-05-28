72462133e97acbd82982bca46807ccc996b32590
Daniel Moraes <124385951+DanielMoraesTI@users.noreply.github.com>
2023-05-28 17:09:42 +0100

    Sem os comentários jo JS

--- app.js
+++ app.js
@@ -11,20 +11,3 @@ let gostaEstudar = prompt('Você gosta de estudar ' + linguagemEstudo + '?' + ' 
   if (gostaEstudar == 2) {
     alert('Ahh que pena... Já tentou aprender outras linguagens?');
   }
-
-
-
-//var numeroSecreto = parseInt(Math.random() * 1001 + 1)
-//var quantidadeTentativas = 1
-//var jogador = prompt('Qual é o seu nome?')
-//while(chute != numeroSecreto) {
-  //var chute = prompt('Digite um número entre 1 e 1000')
-  //if (chute == numeroSecreto) {
-    //alert('Certa resposta ' + jogador + ', agora pode tentar a sorte na Mega Sena! Volte sempre e me deixem críticas contrutivas, obrigado!')
-  //} else if (chute > numeroSecreto) {
-    //alert('Errou... o número secreto é menor do que ' + chute + ', tente novamente!')
-  //} else if (chute < numeroSecreto) {
-    //alert('Errou... o número secreto é maior do que ' + chute + ', tente novamente!')
-  //}
-  //alert('Quantidade de tentaivas: '+ quantidadeTentativas++)
-//}
