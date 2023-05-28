fc0a824fe8e35bcdcb6333002b75ff996beb562d
Daniel Moraes <124385951+DanielMoraesTI@users.noreply.github.com>
2023-05-28 17:09:14 +0100

    JavaScript com EXTRA

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-//console.log('Qual o seu nome?');
-//console.log('Quantos anos você tem?');
-//console.log('Qual linguagem de programação você está estudando?')7
-
 let nome = prompt('Qual o seu nome?');
 let idade = prompt('Quantos anos você tem?');
 let linguagemEstudo = prompt('Qual linguagem de programação você está estudando?');
 
 alert('Olá ' + nome + ', você tem ' + idade + ' anos e já está aprendendo ' + linguagemEstudo + '!');
+
+let gostaEstudar = prompt('Você gosta de estudar ' + linguagemEstudo + '?' + ' Responda com o número 1 para SIM ou 2 para NÃO');
+  if (gostaEstudar == 1) {
+    alert('Muito bom! Continue estudando e você terá muito sucesso.');
+  }
+  if (gostaEstudar == 2) {
+    alert('Ahh que pena... Já tentou aprender outras linguagens?');
+  }
 
 
 
