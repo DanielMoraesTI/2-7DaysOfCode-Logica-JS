4f31eb9a77ca4cfd999f51cd7b3b71c4741ff8fc
Daniel Moraes <124385951+DanielMoraesTI@users.noreply.github.com>
2023-05-29 13:33:58 +0100

    Update app.js com algumas formas de escrita diferentes no alert

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let linguagemEstudo = prompt('Qual linguagem de programação você está estuda
 
 alert('Olá ' + nome + ', você tem ' + idade + ' anos e já está aprendendo ' + linguagemEstudo + '!');
 
+//Outra forma da escrita do alert aparentemente mais simples. Aparentemente iguais em termos práticos de uso (let=var=const)
+//const msg = `"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"!`;
+//alert(msg);
+
 let gostaEstudar = prompt('Você gosta de estudar ' + linguagemEstudo + '?' + ' Responda com o número 1 para SIM ou 2 para NÃO');
   if (gostaEstudar == 1) {
     alert('Muito bom! Continue estudando e você terá muito sucesso.');
